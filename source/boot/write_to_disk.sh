9509520d4b2f8633de0e95bf11a67303432219a9
chuj <1768485949@qq.com>
2021-05-15 01:24:54 -0700

    now we can enter Protection Mode

--- source/boot/write_to_disk.sh
+++ source/boot/write_to_disk.sh
@@ -6,4 +6,4 @@ dd if=./mbr.bin of=../../disk/hd60M.img bs=512 count=1 seek=0 conv=notrunc
 # complie loader
 nasm -I include/ -o loader.bin loader.S
 # write loader
-dd if=./loader.bin of=../../disk/hd60M.img bs=512 count=1 seek=2 conv=notrunc
+dd if=./loader.bin of=../../disk/hd60M.img bs=512 count=4 seek=2 conv=notrunc
