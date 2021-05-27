32aaaa6b7bd74e0a2971bf89f849b0b2869d9aa4
chuj <1768485949@qq.com>
2021-05-27 06:19:56 -0700

    added thread framework

---
+++ source/thread/thread.c
@@ -0,0 +1,51 @@
+#include "thread.h"
+#include "stdint.h"
+#include "string.h"
+#include "global.h"
+#include "memory.h"
+
+#define PAGE_SIZE 4096
+#define STACK_CANARY 0x32512332
+
+static void kernel_thread(thread_func* function, void* func_arg)
+{
+    function(func_arg);
+}
+
+void thread_create(PCB* pthread, thread_func function, void* func_arg)
+{
+    /* reserve the INT_stack's space */
+    pthread->self_kernel_stack -= sizeof(struct INT_stack);
+
+    /* reserve the thread_stack's space */
+    pthread->self_kernel_stack -= sizeof(struct thread_stack);
+    struct thread_stack* kernel_thread = (struct thread_stack*)pthread->self_kernel_stack;
+    kernel_thread->eip = kernel_thread;
+    kernel_thread->function = function;
+    kernel_thread->func_arg = func_arg;
+    kernel_thread->ebp = kernel_thread->ebp = \
+    kernel_thread->esi = kernel_thread->edi = 0;
+}
+
+void init_thread(PCB* pthread, char* name, int priority)
+{
+    memset(pthread, 0, sizeof(*pthread));
+    strcpy(pthread->name, name);
+    pthread->status = TASK_RUNNING;
+    pthread->priority = priority;
+    pthread->self_kernel_stack = (uint32_t*)((uint32_t)pthread + PAGE_SIZE);
+    pthread->canary = STACK_CANARY;
+}
+
+PCB* thread_start(char* name, \
+                  int priority, \
+                  thread_func function, \
+                  void* func_arg)
+{
+    PCB* thread_PCB = kpalloc(1);
+    init_thread(thread_PCB, name, priority);
+    thread_create(thread_PCB, function, func_arg);
+
+    __asm__ volatile ("movl %0, %%esp; pop %%ebp; pop %%ebp; pop %%edi;; pop %%esi; ret": : "g"(thread_PCB->self_kernel_stack) : "memory");
+    return thread_PCB;
+}
---
+++ source/thread/thread.h
@@ -0,0 +1,68 @@
+#ifndef __THREAD_THREAD_H
+#define __THREAD_THREAD_H
+
+#include "stdint.h"
+
+typedef void thread_func(void*);
+
+enum task_status
+{
+    TASK_RUNNING,
+    TASK_READY,
+    TASK_BLOCKD,
+    TASK_WAITING,
+    TASK_HANGING,
+    TASK_DIED
+};
+
+struct INT_stack
+{
+    size_t int_vertor_num;
+    uint16_t gs, __gsh;
+    uint16_t fs, __fsh;
+    uint16_t es, __esh;
+    uint16_t ds, __dsh;
+    uint32_t edi;
+    uint32_t esi;
+    uint32_t ebp;
+    uint32_t esp_dummy;
+    uint32_t ebx;
+    uint32_t edx;
+    uint32_t ecx;
+    uint32_t eax;
+
+    uint32_t err_code;
+    uint32_t eip;
+    uint32_t cs;
+    uint32_t eflags;
+    uint32_t esp_at_signal;
+    uint32_t ss;
+};
+
+struct thread_stack
+{
+    uint32_t ebp;
+    uint32_t ebx;
+    uint32_t edi;
+    uint32_t esi;
+
+    void (*eip) (thread_func* func, void *func_arg);
+
+    /* only used at the first time */
+    void (*dummy_ret);
+    thread_func *function;
+    void *func_arg;
+};
+
+typedef struct task_struct
+{
+    uint32_t *self_kernel_stack;
+    enum task_status status;
+    uint8_t priority;
+    char name[16];
+    uint32_t canary;
+}PCB;
+
+
+
+#endif
